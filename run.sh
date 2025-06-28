6eb62e3c6eeeada45aec2f994620b730ac72f4a7
Sungheyn <rlatjdgus2008@gmail.com>
2025-06-28 19:30:52 +0900

    first commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+set -xue
+
+QEMU=qemu-system-riscv32
+
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c
+$QEMU -machine virt -bios default -serial mon:stdio --no-reboot -nographic \
+    -kernel kernel.elf
